bc5b76af923157a40f9d69117fc015dbb1878f25
Cole Ellison <cellison@squarespace.com>
2020-12-11 09:54:17 -0500

    fix(solutions): cleanup day 11

--- 2020/11_seating.py
+++ 2020/11_seating.py
@@ -12,46 +12,52 @@ def get_input() -> Board:
     return contents
 
 
-empty = "L"
-occ = "#"
-floor = "."
-
+empty, occ, floor = "L", "#", "."
 directions = [(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if (dx, dy) != (0, 0)]
 
 
-def step_game_of_life(board: Board) -> Board:
+def step_game_of_life(board: Board) -> Tuple[Board, bool]:
     new_board = deepcopy(board)
-    board_x, board_y = len(board), len(board[0])
-    for x in range(board_x):
-        for y in range(board_y):
-            occupied_seats = sum([board[x + dx][y + dy] == occ for (dx, dy) in directions])
-            if board[x][y] == empty and occupied_seats == 0:
-                new_board[x][y] = occ
-            elif board[x][y] == occ and occupied_seats >= 4:
-                new_board[x][y] = empty
-    return new_board
-
-
-def step_generous_game(board: Board) -> Board:
-    new_board = deepcopy(board)
-    for x in range(len(board)):
-        for y in range(len(board[0])):
+    changed = False
+    x_range, y_range = range(len(board)), range(len(board[0]))
+    for x in x_range:
+        for y in y_range:
             occupied_seats = sum(
-                [first_visible_is_occupied(board, (x, y), (dx, dy)) for (dx, dy) in directions]
+                [
+                    board[x + dx][y + dy] == occ
+                    for (dx, dy) in directions
+                    if x + dx in x_range and y + dy in y_range
+                ]
             )
             if board[x][y] == empty and occupied_seats == 0:
                 new_board[x][y] = occ
+                changed = True
+            elif board[x][y] == occ and occupied_seats >= 4:
+                new_board[x][y] = empty
+                changed = True
+    return new_board, changed
+
+
+def step_generous_game(board: Board) -> Tuple[Board, bool]:
+    new_board = deepcopy(board)
+    changed = False
+    x_range, y_range = range(len(board)), range(len(board[0]))
+    for x in x_range:
+        for y in y_range:
+            occupied_seats = sum(
+                [first_visible_is_occupied(board, x, y, dx, dy) for (dx, dy) in directions]
+            )
+            if board[x][y] == empty and occupied_seats == 0:
+                new_board[x][y] = occ
+                changed = True
             elif board[x][y] == occ and occupied_seats >= 5:
                 new_board[x][y] = empty
-    return new_board
+                changed = True
+    return new_board, changed
 
 
-def first_visible_is_occupied(
-    board: Board, starting: Tuple[int, int], direction: Tuple[int, int]
-) -> str:
+def first_visible_is_occupied(board: Board, x: int, y: int, dx: int, dy: int) -> str:
     board_x, board_y = range(len(board)), range(len(board[0]))
-    x, y = starting
-    dx, dy = direction
     check_x, check_y = x + dx, y + dy
     while check_x in board_x and check_y in board_y:
         if board[check_x][check_y] != floor:
@@ -62,14 +68,13 @@ def first_visible_is_occupied(
 
 
 if __name__ == "__main__":
-    input = get_input()
-    next_step = step_generous_game(input)
+    board = get_input()
+    board, changed = step_generous_game(board)
 
     count = 0
-    while not next_step == input:
+    while changed == True:
         count += 1
-        input = next_step
-        next_step = step_generous_game(input)
+        board, changed = step_generous_game(board)
 
     print(f"generations: {count}")
-    print(sum([char == occ for line in input for char in line]))
+    print(sum([char == occ for line in board for char in line]))
