617b747c5ad22f201212317aaec9f08d9d7a2850
Cole Ellison <cellison@squarespace.com>
2021-11-29 20:14:21 -0500

    style(rust): run formatter

--- advent-of-rust/src/aoc_2021/day_01.rs
+++ advent-of-rust/src/aoc_2021/day_01.rs
@@ -1,31 +1,37 @@
 #[allow(dead_code)]
 fn part_one(input: &str) -> &str {
-  input
-  // for line in input.lines() {
-  //   print!("{}", line)
-  // }
+    input
+    // for line in input.lines() {
+    //   print!("{}", line)
+    // }
 }
 
 #[allow(dead_code)]
 fn part_two(input: &str) -> &str {
-  input
+    input
 }
 
 #[cfg(test)]
 mod tests {
-  use std::io;
-  use crate::utils::{ get_input, InputType };
-  use super::{ part_one, part_two };
+    use super::{part_one, part_two};
+    use crate::utils::{get_input, InputType};
+    use std::io;
 
-  #[test]
-  fn test_part_one() -> io::Result<()> {
-    assert_eq!(part_one(&get_input(0, 0, InputType::Challenge, 0)?), "test\n");
-    Ok(())
-  }
+    #[test]
+    fn test_part_one() -> io::Result<()> {
+        assert_eq!(
+            part_one(&get_input(0, 0, InputType::Challenge, 0)?),
+            "test\n"
+        );
+        Ok(())
+    }
 
-  #[test]
-  fn test_part_two() -> io::Result<()> {
-    assert_eq!(part_two(&get_input(0, 0, InputType::Challenge, 0)?), "test\n");
-    Ok(())
-  }
+    #[test]
+    fn test_part_two() -> io::Result<()> {
+        assert_eq!(
+            part_two(&get_input(0, 0, InputType::Challenge, 0)?),
+            "test\n"
+        );
+        Ok(())
+    }
 }
