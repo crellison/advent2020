47dd3d0f3dc540abbc6288ee46c6b5151eef8570
Cole Ellison <cellison@squarespace.com>
2021-12-01 08:02:50 -0500

    feat(2021): day 1

--- advent-of-rust/src/aoc_2021/day_01.rs
+++ advent-of-rust/src/aoc_2021/day_01.rs
@@ -1,14 +1,37 @@
 #[allow(dead_code)]
-fn part_one(input: &str) -> &str {
-    input
-    // for line in input.lines() {
-    //   print!("{}", line)
-    // }
+fn part_one(input: &str) -> u16 {
+    let mut increases: u16 = 0;
+    let mut last_depth: u16 = input.lines().next().unwrap().parse().unwrap();
+    for line in input.lines() {
+      let next_depth: u16 = line.parse().unwrap();
+      if next_depth > last_depth {
+          increases += 1;
+      }
+      last_depth = next_depth;
+    }
+    increases
 }
 
 #[allow(dead_code)]
-fn part_two(input: &str) -> &str {
-    input
+fn part_two(input: &str) -> u16 {
+    let mut input_iter = input.lines();
+    let mut increases: u16 = 0;
+    let mut last_three: (u16, u16, u16) = (
+        input_iter.next().unwrap().parse().unwrap(),
+        input_iter.next().unwrap().parse().unwrap(),
+        input_iter.next().unwrap().parse().unwrap(),
+    );
+    let mut last_group_sum = last_three.0 + last_three.1 + last_three.2;
+    for line in input_iter {
+      let next_depth: u16 = line.parse().unwrap();
+      let next_group_sum = last_three.1 + last_three.2 + next_depth;
+      if next_group_sum > last_group_sum {
+          increases += 1;
+      }
+      last_three = (last_three.1, last_three.2, next_depth);
+      last_group_sum = next_group_sum;
+    }
+    increases
 }
 
 #[cfg(test)]
@@ -21,7 +44,7 @@ mod tests {
     fn test_part_one() -> io::Result<()> {
         assert_eq!(
             part_one(&get_input(2021, 1, InputType::Challenge, 0)?),
-            "test\n"
+            1527
         );
         Ok(())
     }
@@ -30,7 +53,7 @@ mod tests {
     fn test_part_two() -> io::Result<()> {
         assert_eq!(
             part_two(&get_input(2021, 1, InputType::Challenge, 0)?),
-            "test\n"
+            1575
         );
         Ok(())
     }
