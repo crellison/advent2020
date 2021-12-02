15dd48b00a92be6d81d61dcc88a9bd912e6ef559
Cole Ellison <cellison@squarespace.com>
2021-12-02 08:24:32 -0500

    feat(2021): day 2

--- advent-of-rust/src/aoc_2021/day_02.rs
+++ advent-of-rust/src/aoc_2021/day_02.rs
@@ -1,14 +1,41 @@
 #[allow(dead_code)]
-fn part_one(input: &str) -> &str {
-    input
-    // for line in input.lines() {
-    //   print!("{}", line)
-    // }
+fn part_one(input: &str) -> i32 {
+    let (mut horizonal, mut depth) = (0, 0);
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split(" ").collect();
+        let dir = tokens[0];
+
+        if let Ok(quant) = tokens[1].parse::<i32>() {
+            match dir {
+                "forward" => horizonal += quant,
+                "down" => depth += quant,
+                "up" => depth -= quant,
+                _ => panic!("Unexpected direction: {}", quant),
+            }
+        }
+    }
+    horizonal * depth
 }
 
 #[allow(dead_code)]
-fn part_two(input: &str) -> &str {
-    input
+fn part_two(input: &str) -> i32 {
+    let (mut horizonal, mut depth, mut aim) = (0, 0, 0);
+    for line in input.lines() {
+        let tokens: Vec<&str> = line.split(" ").collect();
+        let dir = tokens[0];
+        if let Ok(quant) = tokens[1].parse::<i32>() {
+            match dir {
+                "forward" => {
+                    horizonal += quant;
+                    depth += aim * quant;
+                }
+                "down" => aim += quant,
+                "up" => aim -= quant,
+                _ => panic!("Unexpected direction: {}", quant),
+            }
+        }
+    }
+    horizonal * depth
 }
 
 #[cfg(test)]
@@ -18,10 +45,17 @@ mod tests {
     use std::io;
 
     #[test]
+    fn test_examples() -> io::Result<()> {
+        assert_eq!(part_one(&get_input(2021, 2, InputType::Example, 0)?), 150);
+        assert_eq!(part_two(&get_input(2021, 2, InputType::Example, 0)?), 900);
+        Ok(())
+    }
+
+    #[test]
     fn test_part_one() -> io::Result<()> {
         assert_eq!(
             part_one(&get_input(2021, 2, InputType::Challenge, 0)?),
-            "test\n"
+            1762050
         );
         Ok(())
     }
@@ -30,7 +64,7 @@ mod tests {
     fn test_part_two() -> io::Result<()> {
         assert_eq!(
             part_two(&get_input(2021, 2, InputType::Challenge, 0)?),
-            "test\n"
+            1855892637
         );
         Ok(())
     }
