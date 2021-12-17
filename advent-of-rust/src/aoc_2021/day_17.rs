f1634b40363379378cbad17a93831abc8d9d404b
Cole Ellison <cellison@squarespace.com>
2021-12-17 14:57:33 -0500

    feat(2021): day 17

--- advent-of-rust/src/aoc_2021/day_17.rs
+++ advent-of-rust/src/aoc_2021/day_17.rs
@@ -1,25 +1,72 @@
-use crate::utils::{get_input, InputType};
-use std::io;
+use std::{collections::HashSet, io, ops::Range};
+
+// const EXAMPLE: (Range<i32>, Range<i32>) = (20..30 + 1, -10..-5 + 1);
+const CHALLENGE: (Range<i32>, Range<i32>) = (207..263 + 1, -115..-63 + 1);
 
 pub fn main() -> io::Result<()> {
-    println!(
-        "part one: {}",
-        part_one(&get_input(2021, 17, InputType::Challenge, 0)?)
-    );
-    println!(
-        "part two: {}",
-        part_two(&get_input(2021, 17, InputType::Challenge, 0)?)
-    );
+    println!("part one: {:?}", part_one(CHALLENGE));
+    println!("part two: {:?}", part_two(CHALLENGE));
     Ok(())
 }
 
-fn part_one(input: &str) -> &str {
-    input
-    // for line in input.lines() {
-    //   print!("{}", line)
-    // }
+#[derive(Debug, Default)]
+struct ProbeTarget {
+    y_target: Range<i32>,
+    x_target: Range<i32>,
 }
 
-fn part_two(input: &str) -> &str {
-    input
+impl ProbeTarget {
+    fn new(target: &(Range<i32>, Range<i32>)) -> Self {
+        Self {
+            x_target: target.0.clone(),
+            y_target: target.1.clone(),
+        }
+    }
+
+    fn hits(&self) -> HashSet<(i32, i32)> {
+        let mut hits: HashSet<(i32, i32)> = HashSet::new();
+        for dy in self.y_target.start..(-self.y_target.start) {
+            for dx in 0..self.x_target.end {
+                if self.will_hit(dx, dy) {
+                    hits.insert((dx, dy));
+                }
+            }
+        }
+        hits
+    }
+
+    fn will_hit(&self, initial_dx: i32, initial_dy: i32) -> bool {
+        let (mut x,mut y) = (0,0);
+        let (mut dx, mut dy) = (initial_dx, initial_dy);
+
+        while x < self.x_target.end && y > self.y_target.start {
+            if self.x_target.contains(&x) && self.y_target.contains(&y) {
+                return true;
+            }
+            x += dx;
+            y += dy;
+            dx = if dx > 0 { dx - 1 } else { 0 };
+            dy -= 1;
+        }
+
+        self.x_target.contains(&x) && self.y_target.contains(&y)
+
+    }
+}
+
+fn part_one(target: (Range<i32>, Range<i32>)) -> i32 {
+    let mut max = 0;
+    let probe = ProbeTarget::new(&target);
+    for (_, dy) in probe.hits() {
+        let height = dy * (dy + 1) / 2;
+        if max < height {
+            max = height
+        }
+    }
+    max
+}
+
+fn part_two(target: (Range<i32>, Range<i32>)) -> usize {
+    let probe = ProbeTarget::new(&target);
+    probe.hits().len()
 }
