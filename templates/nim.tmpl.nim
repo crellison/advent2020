a4e3f6cbe860c5d8c8978a2f26939516607fd3e2
Cole Ellison <cellison@squarespace.com>
2022-12-02 13:07:36 -0500

    fix(nim): update template to new shape

--- templates/nim.tmpl.nim
+++ templates/nim.tmpl.nim
@@ -1,28 +1,5 @@
-import system/io
+proc partOne*(input: string): string =
+  result = "done one"
 
-proc partOne*(input: string) =
-  echo "done one"
-
-proc partTwo*(input: string) =
-  echo "done two"
-
-proc main() =
-  var exitCode = QuitSuccess
-
-  # relative to package root
-  const input = readFile("./input/${YEAR}/${DAY}-1.txt")
-  for part in [partOne, partTwo]:
-    try:
-      part(input)
-    except Exception as exc:
-      echo "bailing from exception"
-      echo exc.msg
-      exitCode = QuitFailure
-  quit(exitCode)
-
-
-when isMainModule:
-  try:
-    main()
-  except Exception as error:
-    echo error.msg
+proc partTwo*(input: string): string =
+  result = "done two"
