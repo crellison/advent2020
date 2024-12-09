cad6bcb678a29ad113fb849d47d19ff780aeefdb
Cole Ellison <cole.ellison@gmail.com>
2024-12-09 11:57:25 -0500

    build: update python templates

--- templates/python.tmpl.py
+++ templates/python.tmpl.py
@@ -3,11 +3,21 @@ from collections import defaultdict
 from os.path import abspath, dirname
 from typing import List
 
-INPUT_FILE = dirname(abspath(__file__)) + "/inputs/${DAY}-test.txt"
+INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/${YEAR}/${DAY}-test.txt")
 
 def get_input() -> List:
     contents = open(INPUT_FILE).read().split("\n")
+    if contents[-1] == "":
+        contents.pop()
     return contents
+
+def part_one(input: List) -> int:
+    return 0
+
+def part_two(input: List) -> int:
+    return 0
 
 if __name__ == "__main__":
     input = get_input()
+    print(f"Part 1: {part_one(input)}")
+    print(f"Part 2: {part_two(input)}")
