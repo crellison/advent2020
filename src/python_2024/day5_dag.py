4c377cfb0ee55b5a04efc9428f1cd67a8d6f0a8f
Cole Ellison <coleellison@Coles-MacBook-Pro.local>
2024-12-11 10:59:46 -0500

    feat(2024): day 5 and partial 6

--- src/python_2024/day5_dag.py
+++ src/python_2024/day5_dag.py
@@ -1,18 +1,17 @@
-from copy import deepcopy
-from collections import defaultdict
+from functools import cmp_to_key
 from os.path import abspath, dirname
 from typing import List, Tuple
 
-INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/2024/5-test.txt")
+INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/2024/5-1.txt")
 
 
-def get_input() -> Tuple["Dag", List]:
+def get_input() -> Tuple["Graph", List]:
     [edges, paths] = open(INPUT_FILE).read().split("\n\n")
-    dag = Dag()
+    dag = Graph()
     for line in edges.split("\n"):
         start, end = map(int, line.split("|"))
         dag.add_edge(start, end)
-    return dag, paths.split("\n")
+    return dag, [list(map(int, p.split(","))) for p in paths.split("\n") if p != ""]
 
 
 class Node:
@@ -24,9 +23,10 @@ class Node:
         self.deps.add(node.value)
 
 
-class Dag:
-    def __init__(self):
+class Graph:
+    def __init__(self, is_acyclic=False):
         self.nodes = {}
+        self.is_acyclic = is_acyclic
 
     def __add_node(self, value: int):
         if value not in self.nodes:
@@ -41,40 +41,83 @@ class Dag:
         if src in self.nodes[dst].deps:
             return True
 
-        if self.__has_invalid_cycle(src, dst):
-            print(f"Cycle detected: {src} -> {dst}")
+        # this makes it adhere to the DAG constraint
+        if self.is_acyclic and self.__has_invalid_cycle(src, dst):
             return False
 
         self.nodes[src].add_dep(self.nodes[dst])
         return True
+    
+    def has_edge(self, src: int, dst: int) -> bool:
+        return dst in self.nodes[src].deps
 
     def __has_invalid_cycle(self, src: int, dst: int):
         if src not in self.nodes or dst not in self.nodes:
             raise ValueError(f"Node {src} or {dst} does not exist")
 
         seen = set()
-        nodes_to_check = [dst]
+        nodes_to_check = [(dst, [dst])]
         while nodes_to_check:
-            node = nodes_to_check.pop()
+            node, steps = nodes_to_check.pop()
             if node == src:
+                print(f"Cycle detected: {' -> '.join(map(str, steps))}")
                 return True
             if node in seen:
                 continue  # already handled
             seen.add(node)
-            nodes_to_check.extend(self.nodes[node].deps)
+            nodes_to_check.extend((d, [*steps, d]) for d in self.nodes[node].deps)
         return False
+    
+    def is_valid_ordering(self, path: List[int]) -> bool:
+        seen = set()
+        for node_id in path:
+            if node_id not in self.nodes:
+                raise ValueError(f"Node {node_id} does not exist")
+            if any(dep in seen for dep in self.nodes[node_id].deps):
+                return False
+            seen.add(node_id)
+        return True
+    
+    def order_dag_nodes(self) -> List[int]:
+        if not self.is_acyclic:
+            raise ValueError("Graph is not acyclic. Cannot order nodes.")
+        seen = set()
+        ordered_nodes = []
+        def visit_node(node_id: int):
+            if node_id in seen:
+                return
+            seen.add(node_id)
+            for dep in self.nodes[node_id].deps:
+                visit_node(dep)
+            ordered_nodes.append(node_id)
+
+        for node in self.nodes.values():
+            if node.value in seen:
+                continue
+            visit_node(node.value)
+        return ordered_nodes
 
 
-def part_one(dag: Dag, paths: List) -> int:
-    print("\n".join(f"{n.value} <- {n.deps}" for n in dag.nodes.values()))
-    return 0
+def part_one(dag: Graph, paths: List) -> int:
+    mid_path_sum = 0
+    for path in paths:
+        if dag.is_valid_ordering(path):
+            mid_path_sum += path[len(path) // 2]
+    return mid_path_sum
 
 
-def part_two(paths: List) -> int:
-    return 0
+def part_two(dag: Graph, paths: List) -> int:
+    mid_path_sum = 0
+    for path in paths:
+        if dag.is_valid_ordering(path):
+            continue
+        sorted_path = sorted(path, key=cmp_to_key(lambda x, y: -1 if dag.has_edge(x, y) else 1 if dag.has_edge(y, x) else 0))
+        mid_path_sum += sorted_path[len(sorted_path) // 2]
+    return mid_path_sum
 
 
 if __name__ == "__main__":
     dag, paths = get_input()
+    # print("\n".join(f"{n.value} <- {n.deps}" for n in dag.nodes.values()))
     print(f"Part 1: {part_one(dag, paths)}")
-    print(f"Part 2: {part_two(paths)}")
+    print(f"Part 2: {part_two(dag, paths)}")
---
+++ src/python_2024/day6_paths.py
@@ -0,0 +1,142 @@
+import sys
+from copy import deepcopy
+from collections import defaultdict
+from os.path import abspath, dirname
+from typing import List, Tuple
+
+INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/2024/6-1.txt")
+
+# order of directions (row, col) in up, right, down, left
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+def get_input():
+    start = (-1, -1)
+    obstacles = defaultdict(lambda: defaultdict(bool))
+    contents = open(INPUT_FILE).read().split("\n")
+    for j in range(-1, len(contents[0]) + 1):
+        obstacles[-1][j] = True
+        obstacles[len(contents)][j] = True
+    for i, line in enumerate(contents):
+        obstacles[i][-1] = True
+        obstacles[i][len(line)] = True
+        for j, char in enumerate(line):
+            if char == "^":
+                start = (i, j)
+            elif char == "#":
+                obstacles[i][j] = True
+
+    if start == (-1, -1):
+        raise ValueError("Start not found")
+    return start, obstacles
+
+
+def get_next_pos(pos: Tuple[int, int], obstacles, direction: str):
+    possible_positions = (
+        [
+            (pos[0], x)
+            for x in obstacles[pos[0]].keys()
+            if (x > pos[1] if direction[1] == 1 else x < pos[1])
+        ]
+        if direction[0] == 0
+        else [
+            (x, pos[1])
+            for x in obstacles.keys()
+            if pos[1] in obstacles[x]
+            and (x > pos[0] if direction[0] == 1 else x < pos[0])
+        ]
+    )
+    next_obstacle = min(
+        possible_positions, key=lambda x: abs(x[0] - pos[0]) + abs(x[1] - pos[1])
+    )
+    # back up one step to not hit the obstacle
+    return (
+        next_obstacle,
+        (next_obstacle[0] - direction[0], next_obstacle[1] - direction[1]),
+    )
+
+
+def part_one(start: Tuple[int, int], obstacles: defaultdict) -> int:
+    visited = set()
+    visited.add(start)
+    dir_index = 0
+    pos = start
+    for i in range(1000):  # no infinite loops
+        next_obstacle, next_pos = get_next_pos(pos, obstacles, directions[dir_index])
+        # print(f"pos: {pos}, next_pos: {next_pos}, next_obstacle: {next_obstacle}")
+        di, dj = directions[dir_index]
+        for i in range(abs(next_obstacle[0] - pos[0]) + abs(next_obstacle[1] - pos[1])):
+            visited.add((pos[0] + di * i, pos[1] + dj * i))
+        pos = next_pos
+        dir_index = (dir_index + 1) % 4
+        if (
+            next_obstacle[0] == min(obstacles.keys())
+            or next_obstacle[0] == max(obstacles.keys())
+            or next_obstacle[1] == min(obstacles[-1].keys())
+            or next_obstacle[1] == max(obstacles[-1].keys())
+        ):
+            break
+    return len(visited)
+
+
+def part_two(start: Tuple[int, int], obstacles: defaultdict) -> int:
+    possible_obstacles = [
+        (i, j)
+        for i in range(min(obstacles.keys()) + 1, max(obstacles.keys()))
+        for j in range(min(obstacles[i].keys()) + 1, max(obstacles[i].keys()))
+        if j not in obstacles[i] and (i, j) != start
+    ]
+    valid_obstacle_count = 0
+
+    def check_for_loop(obstacles):
+        visited = set()
+        visited.add(start)
+        dir_index = 0
+        pos = start
+        possible_loop = False
+        for i in range(1, 1000):  # no infinite loops
+            next_obstacle, next_pos = get_next_pos(
+                pos, obstacles, directions[dir_index]
+            )
+            distance_from_current = abs(next_obstacle[0] - pos[0]) + abs(
+                next_obstacle[1] - pos[1]
+            )
+            if next_pos in visited and distance_from_current > 1:
+                if not possible_loop:
+                    # need to validate that the step after next is in visited
+                    possible_loop = True
+                else:
+                    # print(f"loop found after step {i} with next position {next_pos}")
+                    return True
+
+            # print(f"pos: {pos}, next_pos: {next_pos}, next_obstacle: {next_obstacle}")
+            di, dj = directions[dir_index]
+            dir_index = (dir_index + 1) % 4
+
+            for i in range(distance_from_current):
+                visited.add((pos[0] + di * i, pos[1] + dj * i))
+            pos = next_pos
+            if (
+                next_obstacle[0] == min(obstacles.keys())
+                or next_obstacle[0] == max(obstacles.keys())
+                or next_obstacle[1] == min(obstacles[-1].keys())
+                or next_obstacle[1] == max(obstacles[-1].keys())
+            ):
+                return False
+
+    for i, j in possible_obstacles:
+        obstacles[i][j] = True
+        if check_for_loop(obstacles):
+            # print(f"loop found with obstacle at {i}, {j}")
+            valid_obstacle_count += 1
+        del obstacles[i][j]
+
+    return valid_obstacle_count
+
+
+if __name__ == "__main__":
+    start, obstacles = get_input()
+    print(f"Part 1: {part_one(start, obstacles)}")
+    print(f"Part 2: {part_two(start, obstacles)}")
+    # 900, 901 too low
+    # 1578 too high
