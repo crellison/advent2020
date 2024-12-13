a08560a0a8800588c68ceadf9e6a88b60a05cdcf
Cole Ellison <cole.ellison@gmail.com>
2024-12-12 20:06:02 -0500

    feat(2024): more progress on 9

--- src/python_2024/day9_checksum.py
+++ src/python_2024/day9_checksum.py
@@ -2,8 +2,9 @@ from copy import deepcopy
 from collections import defaultdict
 from os.path import abspath, dirname
 from typing import List
+import heapq
 
-INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/2024/9-test.txt")
+INPUT_FILE = abspath(dirname(abspath(__file__)) + "/../../input/2024/9-1.txt")
 
 
 def get_input() -> str:
@@ -49,34 +50,59 @@ def part_one(input: str) -> int:
     return checksum
 
 
+"""
+blocks [(id, start, len)]
+possible to keep track of empty blocks by size?
+empty_blocks {
+    size: min_queue(start_index)
+}
+
+
+"""
+
+
 def part_two(input: str) -> int:
     """
     move files to forwardmost position they fit from back to front without breaking up files
     """
+    files = []  # (id, start_position, length)
+    empty_blocks = defaultdict(list)  # (length, start_position) managed by heapq
     block_position = 0
-    tail_cursor = len(input) - 1 if len(input) % 2 == 1 else len(input) - 2
-    tail_blocks = int(input[tail_cursor])
-    checksum = 0
     for i, num in enumerate(input):
         block_len = int(num)
-        if i == tail_cursor:
-            for _ in range(tail_blocks):
-                checksum += block_position * (tail_cursor // 2)
-                block_position += 1
-            break
-        if i % 2 == 0 and i != tail_cursor:  # processing a file
-            for _ in range(block_len):
-                checksum += block_position * (i // 2)
-                block_position += 1
-        else:  # processing empty space
-            while 
-            for _ in range(block_len):
-                if tail_blocks == 0:
-                    tail_cursor -= 2
-                    tail_blocks = int(input[tail_cursor])
-                checksum += block_position * (tail_cursor // 2)
-                tail_blocks -= 1
-                block_position += 1
+        if i % 2 == 0:
+            files.append((i // 2, block_position, block_len))
+        else:
+            heapq.heappush(empty_blocks[block_len], block_position)
+        block_position += block_len
+
+    for i in range(len(files) - 1, -1, -1):
+        id, start_position, file_len = files[i]
+        relocation_slot_size = min(
+            (
+                x
+                for x in empty_blocks.keys()
+                if x >= file_len and len(empty_blocks[x]) > 0
+            ),
+            default=-1,
+        )
+        if relocation_slot_size == -1:
+            continue
+        relocation_slot_start = heapq.heappop(empty_blocks[relocation_slot_size])
+        if relocation_slot_start >= start_position:
+            continue
+        # print(f"moving file {id} from {start_position} to {relocation_slot_start}")
+        files[i] = (id, relocation_slot_start, file_len)
+        heapq.heappush(
+            empty_blocks[relocation_slot_size - file_len],
+            relocation_slot_start + file_len,
+        )
+
+    checksum = 0
+    for id, start_position, file_len in files:
+        for i in range(file_len):
+            checksum += id * (start_position + i)
+
     return checksum
 
 
@@ -84,3 +110,5 @@ if __name__ == "__main__":
     input = get_input()
     print(f"Part 1: {part_one(input)}")
     print(f"Part 2: {part_two(input)}")
+    # 6254536635297 too low
+    # 8449596564468 too high
